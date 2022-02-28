fd3d903f4c13cca61ad4517ed19b727aa5f28833
Hao Xu <39208453+RyanHXu@users.noreply.github.com>
2022-02-28 11:28:21 -0600

    subset II

--- backtrack/90.subsets-ii.cpp
+++ backtrack/90.subsets-ii.cpp
@@ -15,22 +15,26 @@ public:
   vector<vector<int>> subsetsWithDup(vector<int> &nums) {
     vector<vector<int>> res{{}};
     sort(nums.begin(), nums.end());
-    int start,end=0;
+    int start, end = 0;
     vector<int> temp;
-    for (int i = 0; i < nums.size(); ++i)
-    {
-        start = 0;
-        if (i>0 && nums[i] == nums[i-1]){
-            start = end + 1;
-        }
-        end = res.size() - 1;
-        for (int j = start; j <=end; ++j) {
-            temp = res[j];
-            temp.push_back(nums[i]);
-            res.push_back(temp);
-        }
+    for (int i = 0; i < nums.size(); ++i) {
+      start = 0;
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        start = end + 1;
+      }
+      end = res.size() - 1;
+      for (int j = start; j <= end; ++j) {
+        temp = res[j];
+        temp.push_back(nums[i]);
+        res.push_back(temp);
+      }
     }
     return res;
   }
 };
 // @lc code=end
+int main() {
+  Solution s;
+  vector<int> ivec{1, 2, 2};
+  s.subsetsWithDup(ivec);
+}
