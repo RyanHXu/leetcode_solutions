7bb8ef2f9bb9a8dc6ec05d80b1d237fb27777573
RyanHXu <yeweiyang98@gmail.com>
2022-02-27 18:24:13 -0600

    Combination Sum II

--- backtrack/40.combination-sum-ii.cpp
+++ backtrack/40.combination-sum-ii.cpp
@@ -9,7 +9,40 @@
 using namespace std;
 
 // @lc code=start
+class Solution {
+  vector<vector<int>> res;
+  vector<int> comb;
 
+public:
+  vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+    sort(candidates.begin(), candidates.end());
+    backtrack(candidates, target, 0);
+    return res;
+  }
+  void backtrack(vector<int> &candidates, int target, int start) {
+    if (target == 0) {
+      res.push_back(comb);
+      return;
+    }
+
+    for (int i = start; i < candidates.size(); ++i) {
+      // NOTE:
+      if (i>start && candidates[i] == candidates[i-1]){
+        continue;
+      }
+
+      int pick = candidates[i];
+      // optimization: early stopping
+      if (target - pick < 0) {
+        break;
+      }
+
+      comb.push_back(pick);
+      backtrack(candidates, target - pick, i + 1);
+      comb.pop_back();
+    }
+  }
+};
 
 // @lc code=end
 
