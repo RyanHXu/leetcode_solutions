7d8f5f336ccfc65254b3020c925b2815c0aa8b81
RyanHXu <yeweiyang98@gmail.com>
2022-03-18 01:40:05 -0500

    Delete and Earn - button up space optimized

--- dp/day3/740.delete-and-earn.cpp
+++ dp/day3/740.delete-and-earn.cpp
@@ -9,8 +9,65 @@
 using namespace std;
 
 // @lc code=start
-
+// ButtonUpOpimizeSpace
 class Solution {
+
+public:
+  int deleteAndEarn(vector<int> &nums) {
+    unordered_map<int, int> points;
+
+    int maxElement = 0;
+    int n = nums.size();
+    for (const auto &val : nums)
+    {
+        points[val] += val;
+        maxElement = max(maxElement, val);
+    }
+
+    int oneBack = points.count(1)?points[1]:0;
+    int twoBack = 0;
+
+    // start from 1
+    for (int i = 2; i < maxElement+1; ++i) {
+      int val = points.count(i) ? points[i] : 0;
+      int temp = oneBack;
+      oneBack = max(oneBack, twoBack +val);
+      twoBack = temp;
+    }
+    return oneBack;
+  }
+};
+// @lc code=end
+
+
+class SolutionButtonUp {
+
+public:
+  int deleteAndEarn(vector<int> &nums) {
+    unordered_map<int, int> points;
+
+    int maxElement = 0;
+    int n = nums.size();
+    for (const auto &val : nums)
+    {
+        points[val] += val;
+        maxElement = max(maxElement, val);
+    }
+
+    vector<int> dp(maxElement + 1, 0);
+    dp[1] = points[1];
+
+
+    // start from 1
+    for (int i = 2; i < maxElement+1; ++i) {
+        int curr = points.count(i) ? points[i] : 0;
+        dp[i] = max(dp[i - 1], dp[i - 2] + curr);
+    }
+    return dp[maxElement];
+  }
+};
+
+class SolutionTopDown {
 
   unordered_map<int, int> points;
   unordered_map<int, int> dp;
@@ -30,18 +87,16 @@ public:
       return 0;
     }
     if (dp.find(num) == dp.end()) {
-        int curr = points.count(num) ? points[num] : 0;
-        dp[num] = max(deleteAndEarnTopDown(num - 2) + curr,
-                      deleteAndEarnTopDown(num - 1));
+      int curr = points.count(num) ? points[num] : 0;
+      dp[num] = max(deleteAndEarnTopDown(num - 2) + curr,
+                    deleteAndEarnTopDown(num - 1));
     }
     return dp[num];
   }
 };
-// @lc code=end
-
 int main() {
   // c++ map default value
-//   unordered_map<int, int> m;
+  //   unordered_map<int, int> m;
   //   for (int i = 0; i < 10; ++i) {
   //     m[i] += i + 10;
   //   }
@@ -67,6 +122,6 @@ int main() {
   //   cout << m[100000] << endl;
   //   cout << m.size() << endl;
   Solution s;
-  vector<int> nums{2,2,3,3,3,4};
+  vector<int> nums{2, 2, 3, 3, 3, 4};
   cout << s.deleteAndEarn(nums) << endl;
 }
