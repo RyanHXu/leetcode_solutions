cd02a6f83c3ff9c729ffb3ef4f32a82785c6d639
RyanHXu <yeweiyang98@gmail.com>
2022-03-19 20:59:30 -0500

    stock with cooldown

--- dp/day7_best_time_to_buyAndSell/309.best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ dp/day7_best_time_to_buyAndSell/309.best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -10,10 +10,27 @@ using namespace std;
 
 
 // @lc code=start
+/**
+hold[i] = max(hold[i-1], rest[i-1]-prices[i])
+sold[i] = hold[i-1] + prices[i]
+rest[i] = max(rest[i-1],sold[i-1])
+
+init: rest[0] = sold[0] =0, hold[0] = INT32_MIN
+ans: max(rest[i],sold[i])
+ */
+
+// https://www.youtube.com/watch?v=oL6mRyTn56M
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int rest=0, sold=0,hold=INT32_MIN;
+        for (const auto& price : prices) {
+            hold = max(hold, rest - price);
+            sold = hold + price;
+            rest = max(hold, rest - price);
+        }
+        return max(rest, sold);
     }
 };
 // @lc code=end
