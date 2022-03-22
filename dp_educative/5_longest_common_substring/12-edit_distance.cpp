b356120548867c14620c37d54b17c44a506f3ad1
Hao Xu <39208453+RyanHXu@users.noreply.github.com>
2022-03-22 15:59:16 -0500

    LCS 12/13 brute force done

--- dp_educative/5_longest_common_substring/12-edit_distance.cpp
+++ dp_educative/5_longest_common_substring/12-edit_distance.cpp
@@ -2,14 +2,37 @@
 
 using namespace std;
 
-
-
+// brute force idea
+// try all three operations and iterate s1 and s2
+// 1) if s1[i]==s2[j], we recursively compare the remaining part
+// 2) if s1[i]!=s2[j], we call delete, insert, replace opetions recursively
 class EditDistance {
 
 public:
   int findMinOperations(const string &s1, const string &s2) {
-      
-    return -1;
+
+    return findMinOperations(s1, s2, 0, 0);
+  }
+
+  int findMinOperations(const string &s1, const string &s2, int i, int j) {
+    if (i == s1.length()) {
+      return s2.length() - j;
+    }
+
+    if (j == s2.length()) {
+      return s1.length() - i;
+    }
+
+    int c1 = 0, c2 = 0, c3 = 0;
+    if (s1[i] == s2[j]) {
+      return findMinOperations(s1, s2, i + 1, j + 1);
+    }
+
+    c1 = 1 + findMinOperations(s1, s2, i + 1, j);     // perform deletion
+    c2 = 1 + findMinOperations(s1, s2, i, j + 1);     // perform insertion
+    c3 = 1 + findMinOperations(s1, s2, i + 1, j + 1); // perform replace
+
+    return min(c1, min(c2, c3));
   }
 };
 
